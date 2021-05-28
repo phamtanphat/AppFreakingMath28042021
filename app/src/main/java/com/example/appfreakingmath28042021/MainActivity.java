6ad3dd5c81e5a6e665f379b31504deb9d0f3e425
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-05-28 20:10:16 +0700

    random

--- app/src/main/java/com/example/appfreakingmath28042021/MainActivity.java
+++ app/src/main/java/com/example/appfreakingmath28042021/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 
 import java.util.Map;
+import java.util.Random;
+import java.util.regex.Matcher;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +22,23 @@ public class MainActivity extends AppCompatActivity {
 //        double number2 = Math.floor(1.999999);
 //        Log.d("BBB", number2 + "");
         // Làm tròn lên
-        double number3 = Math.ceil(1.0001);
-        Log.d("BBB", number3 + "");
+//        double number3 = Math.ceil(1.0001);
+//        Log.d("BBB", number3 + "");
+        // Random
+//        double number4 = Math.random();
+//        Log.d("BBB", number4 + "");
+
+        // Builtin - function
+//        double number5 =  Math.round(Math.random() * (10 - 5)) + 5;
+//        Log.d("BBB",number5 + "");
+        // 5 - 10
+        Random random = new Random();
+        for (int i = 0; i < 50 ; i++) {
+            // (smax - smin + 1) + smin
+            int number6 = random.nextInt(10 - 5 + 1) + 5;
+            Log.d("BBB",number6 + "");
+        }
+
+
     }
 }
