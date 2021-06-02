624b68649a67dfd958788dc039c6f2fe2a39bb8a
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-06-02 20:09:06 +0700

    xu ly su kien cho button

--- app/src/main/java/com/example/appfreakingmath28042021/MainActivity.java
+++ app/src/main/java/com/example/appfreakingmath28042021/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Map;
 import java.util.Random;
@@ -15,10 +17,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     TextView mTvScore, mTvCalculator, mTvResult;
-    ImageButton mImgBtnWin, mImgBtnLose;
+    ImageButton mImgBtnTrue, mImgBtnFalse;
     int mNumber1 = 0, mNumber2 = 0, mIndexCalculator = -1, mResult = -1;
     boolean mIsTrue;
     Random mRandom;
+    int mScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +31,32 @@ public class MainActivity extends AppCompatActivity {
         mTvScore = findViewById(R.id.textViewScore);
         mTvCalculator = findViewById(R.id.textViewCalculator);
         mTvResult = findViewById(R.id.textViewResult);
-        mImgBtnLose = findViewById(R.id.imageLose);
-        mImgBtnWin = findViewById(R.id.imageWin);
+        mImgBtnTrue = findViewById(R.id.imageWin);
+        mImgBtnFalse = findViewById(R.id.imageLose);
 
         mRandom = new Random();
 
+        progressCalculator();
+
+        // random 2 số ngẫu nhiên
+        // random 1 toán tử ngẫu nhiên
+        // random kết quả
+        mImgBtnTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mIsTrue){
+                    mScore = 0;
+                    Toast.makeText(MainActivity.this, "Thua rồi", Toast.LENGTH_SHORT).show();
+                }else{
+                    mScore++;
+                    progressCalculator();
+                }
+
+                mTvScore.setText(mScore + "");
+            }
+        });
+    }
+    private void progressCalculator(){
         mNumber1 = mRandom.nextInt(9) + 1;
         mNumber2 = mRandom.nextInt(9) + 1;
         mIsTrue = mRandom.nextBoolean();
@@ -62,10 +86,5 @@ public class MainActivity extends AppCompatActivity {
             mResult += mRandom.nextInt(10) + 1;
         }
         mTvResult.setText(" = " + mResult);
-        // random 2 số ngẫu nhiên
-        // random 1 toán tử ngẫu nhiên
-        // random kết quả
-
-
     }
 }
